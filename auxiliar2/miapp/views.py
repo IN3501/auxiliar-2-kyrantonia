7a2e7eaf9b02a084ebc036c6db7ff6f36fb260e2
kyra <kyra.cossio@ug.uchile.cl>
2019-08-13 17:42:08 -0400

    proyecto

---
+++ auxiliar2/miapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'miapp/index.html')
+def pestaña(request):
+    return render(request,'miapp/pestaña.html')
+def integrantes(request):
+    return render(request,'miapp/integrantes.html')
